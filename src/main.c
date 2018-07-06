dbddf74ddb3c455087555704c11427d558b72e92
maya2250 <maya2250.dev@gmail.com>
2018-07-06 20:40:55 +0900

    Add create table sample

--- src/main.c
+++ src/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <sqlite3.h>
 
 int main(){
-  printf("Hello World!\n");
+  sqlite3 *db;
+
+  if (sqlite3_open("demo.sqlite3", &db) != SQLITE_OK) {
+    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return 1;
+  }
+
+  char* createSql = "CREATE TABLE demo (id integer);";
+
+  if(sqlite3_exec(db, createSql, 0, 0, NULL) != SQLITE_OK) {
+    fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+  }
+
+  sqlite3_close(db);
+
   return 0;
 }
